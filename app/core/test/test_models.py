47f57c855396d893d0a9b58e09eed0fcc64dd1aa
khusanabdukarimov007 <khusanabdukarimov007@gmail.com>
2023-01-25 21:31:16 +0500

    figure out mistakes

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -4,6 +4,9 @@ Tests for models
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from decimal import Decimal
+
+from core import models
 
 
 class ModulTests(TestCase):
@@ -34,7 +37,7 @@ class ModulTests(TestCase):
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
-        """Test that creating a user without an emal raises a ValueError"""
+        """Test that creating a user without an email raises a ValueError"""
 
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
@@ -48,3 +51,20 @@ class ModulTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test create  a recipe is successful"""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123',
+        )
+
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample recipe description'
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
