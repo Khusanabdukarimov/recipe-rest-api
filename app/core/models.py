c2f58007755c02af3b10bd65845e4686589f8ac9
khusanabdukarimov007 <khusanabdukarimov007@gmail.com>
2023-01-25 09:53:01 +0500

    added user api

--- app/core/models.py
+++ app/core/models.py
@@ -4,7 +4,7 @@ Database models
 
 from django.db import models
 from django.contrib.auth.models import (
-    AbstractUser,
+    AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin
 )
@@ -14,14 +14,27 @@ class UserManager(BaseUserManager):
     """Manager for user"""
 
     def create_user(self, email, password=None, **extra_fields):
-        """create, save and return a new user"""
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+
         user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
         return user
 
 
-class User(AbstractUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
@@ -31,4 +44,3 @@ class User(AbstractUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
