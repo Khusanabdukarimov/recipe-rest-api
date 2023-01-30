65e900e5d932a0882bfaab563ee168ae309157c7
khusanabdukarimov007 <khusanabdukarimov007@gmail.com>
2023-01-30 16:15:06 +0500

    added tag model

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -9,6 +9,12 @@ from decimal import Decimal
 from core import models
 
 
+def create_user(email='user@example.com', password='test_password'):
+    """Create and return new user"""
+
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModulTests(TestCase):
     """ Test models"""
 
@@ -68,3 +74,10 @@ class ModulTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
